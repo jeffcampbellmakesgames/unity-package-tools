7a91a86d129bf051c47a7977b789e73ddc33966d
Jeff Campbell <jeff@zapdot.com>
2020-03-09 12:26:32 +0100

    Created about window with support, donate info
    * Created menu items for easily accessing Github Issues page, KOFI for support, etc...
    * Created about window to show version information about the library as well as developer information.

---
+++ Unity/Assets/JCMG/PackageTools/Scripts/Editor/MenuItems.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JCMG.PackageTools.Editor
+{
+	internal static class MenuItems
+	{
+		[MenuItem("Tools/JCMG/PackageTools/Submit bug or feature request")]
+		internal static void OpenURLToGitHubIssuesSection()
+		{
+			const string GITHUB_ISSUES_URL = "https://github.com/jeffcampbellmakesgames/unity-package-tools/issues";
+
+			Application.OpenURL(GITHUB_ISSUES_URL);
+		}
+
+		[MenuItem("Tools/JCMG/PackageTools/Donate to support development")]
+		internal static void OpenURLToKoFi()
+		{
+			const string KOFI_URL = "https://ko-fi.com/stampyturtle";
+
+			Application.OpenURL(KOFI_URL);
+		}
+
+		[MenuItem("Tools/JCMG/PackageTools/About")]
+		internal static void OpenAboutModalDialog()
+		{
+			AboutWindow.View();
+		}
+	}
+}
---
+++ Unity/Assets/JCMG/PackageTools/Scripts/Editor/VersionConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace JCMG.PackageTools.Editor
+{
+	/// <summary>
+	/// Version info for this library.
+	/// </summary>
+	internal static class VersionConstants
+	{
+		// Version
+		public const string VERSION = "1.2.1";
+	}
+}
