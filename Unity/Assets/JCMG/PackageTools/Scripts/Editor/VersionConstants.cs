0a7ff954c3ef65b3c5537524af4979567135a080
Jeff Campbell <jeff@zapdot.com>
2020-05-01 10:28:49 +0200

    Bumped package version to v1.3.0

--- Unity/Assets/JCMG/PackageTools/Scripts/Editor/VersionConstants.cs
+++ Unity/Assets/JCMG/PackageTools/Scripts/Editor/VersionConstants.cs
@@ -1,11 +1,33 @@
-﻿namespace JCMG.PackageTools.Editor
+namespace JCMG.PackageTools.Editor
 {
 	/// <summary>
 	/// Version info for this library.
 	/// </summary>
 	internal static class VersionConstants
 	{
-		// Version
-		public const string VERSION = "1.2.1";
+		/// <summary>
+		/// The semantic version
+		/// </summary>
+		public const string VERSION = "1.3.0";
+
+		/// <summary>
+		/// The branch of GIT this package was published from.
+		/// </summary>
+		public const string GIT_BRANCH = "develop";
+
+		/// <summary>
+		/// The current GIT commit hash this package was published on.
+		/// </summary>
+		public const string GIT_COMMIT = "57aec574ed19746de42ffa5032358562fb041ebf";
+
+		/// <summary>
+		/// The UTC human-readable date this package was published at.
+		/// </summary>
+		public const string PUBLISH_DATE = "Friday, May 1, 2020";
+
+		/// <summary>
+		/// The UTC time this package was published at.
+		/// </summary>
+		public const string PUBLISH_TIME = "05/01/2020 08:26:31";
 	}
 }
