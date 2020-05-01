0e51b7251040e01c74897f62487232a04024ab00
Jeff Campbell <jeff@zapdot.com>
2020-05-01 10:46:24 +0200

    v1.3.1
    # v1.3.1
    
    # ADDED
    * Updated or added all license headers to reflect MIT license.

--- PackageTools/Scripts/Editor/VersionConstants.cs
+++ PackageTools/Scripts/Editor/VersionConstants.cs
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2020 Jeff Campbell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 namespace JCMG.PackageTools.Editor
 {
 	/// <summary>
@@ -8,7 +32,7 @@ namespace JCMG.PackageTools.Editor
 		/// <summary>
 		/// The semantic version
 		/// </summary>
-		public const string VERSION = "1.3.0";
+		public const string VERSION = "1.3.1";
 
 		/// <summary>
 		/// The branch of GIT this package was published from.
@@ -18,7 +42,7 @@ namespace JCMG.PackageTools.Editor
 		/// <summary>
 		/// The current GIT commit hash this package was published on.
 		/// </summary>
-		public const string GIT_COMMIT = "57aec574ed19746de42ffa5032358562fb041ebf";
+		public const string GIT_COMMIT = "d3459e0890edfd0ef5a10ff78dc451e8c07e284f";
 
 		/// <summary>
 		/// The UTC human-readable date this package was published at.
@@ -28,6 +52,6 @@ namespace JCMG.PackageTools.Editor
 		/// <summary>
 		/// The UTC time this package was published at.
 		/// </summary>
-		public const string PUBLISH_TIME = "05/01/2020 08:26:31";
+		public const string PUBLISH_TIME = "05/01/2020 08:44:30";
 	}
 }
