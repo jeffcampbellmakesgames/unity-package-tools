c83f29e45c938bfc95b810c3661d3e037d315dc2
Jeff Campbell <jeff@zapdot.com>
2020-11-01 11:15:17 -0500

    Updated license headers

--- Unity/Assets/JCMG/PackageTools/Scripts/Editor/AssemblyInfo.cs
+++ Unity/Assets/JCMG/PackageTools/Scripts/Editor/AssemblyInfo.cs
@@ -21,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
 using System.Runtime.CompilerServices;
 
 // Internals are exposed to this assembly to allow for unit-tests of non-user facing functionality
